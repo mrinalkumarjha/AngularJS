4e00665b4bca4feab5c5dace45e3a6db0beacd5d
mrinalkumar <mrinal.kumar@britishcouncil.org>
2020-03-22 11:17:51 +0530

    Adding complex type to controller like employee, also used method chaining in script.js

--- AngularJsDemo/AngularJsDemo/Scripts/Script.js
+++ AngularJsDemo/AngularJsDemo/Scripts/Script.js
@@ -4,14 +4,38 @@
 
 // creating module
 // then we need to associate this module with ng-app directive
-var myapp = angular.module("myModule", []); 
+//var myapp = angular.module("myModule", []); 
 
 
-// Creating controller
-// then we need to associate this controller with ng-controller directive, and we do that in view.
-var myController = function ($scope) {
-    $scope.messgae = "Angular js tutorial";
-}
+//// Creating controller
+//// then we need to associate this controller with ng-controller directive, and we do that in view.
+//var myController = function ($scope) {
+//    var employee = {
+//        FirstName: 'David',
+//        LastName: 'hasting',
+//        Gender: 'Male'
 
-// Registring controller.
-myapp.controller("myController", myController);
+//    }
+//    $scope.employee = employee;
+//}
+
+//// Registring controller.
+//myapp.controller("myController", myController);
+
+
+
+// creating module controller and registering in one line using method chaining.
+// either you can use above method or this one using method chaining.
+var myapp = angular
+    .module("myModule", [])
+    .controller("myController", function ($scope) {
+
+        var employee = {
+            FirstName: 'David',
+            LastName: 'hasting',
+            Gender: 'Male'
+
+        }
+        $scope.employee = employee;
+    });
+    
